d3aa2487081c3f6be84c5568c68ddf8f7e2dbe90
giljihun <accc45@outlook.com>
2023-10-17 14:50:20 +0900

    Initial Commit

---
+++ DuriAnyujin/DuriAnyujinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuriAnyujinApp.swift
+//  DuriAnyujin
+//
+//  Created by mobicom on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct DuriAnyujinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
